da780f2e9e33130e7b88d1219a5e2a827d32eb99
TadasVai <tadas.vaiciulenas@stud.vilniustech.lt>
2023-04-20 19:33:41 +0300

    Validation and creating partials

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const router = express.Router();
 
+//Welcome page
 router.get('/', (req, res) => res.render('Welcome'));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,39 @@ router.get('/login', (req, res) => res.render('Login'));
 //Login Page
 router.get('/register', (req, res) => res.render('Register'));
 
+// Register Handle
+router.post('/register', (req, res) => {
+    const{name, email, password, password2} = req.body;
+let errors =[];
+
+// Check require fields
+if(!name || !email || !password ||!password2){
+    errors.push({msg: 'Please fill in all fields'});
+}
+
+// Check passwords match
+if(password != password2){
+    errors.push({msg: 'Passwords do not match!'});
+}
+
+// Check password length
+if(password.length < 6){
+    errors.push({msg: 'Password is too short! Should be at least 6 characters'});
+}
+
+if(errors.length > 0){
+    res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+    })
+} else{
+    res.send('pass');
+}
+
+
+});
+
 module.exports = router; 
